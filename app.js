f0146d4773840126397ad8f281d3d5fd1bae84ad
tyler <tyler.dojcak@gmail.com>
2021-03-15 15:36:51 -0400

    function to get a new 5-card hand

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 let deckID;
+let hand;
 
 function getNewDeck() {
     fetch("https://deckofcardsapi.com/api/deck/new/shuffle/")
@@ -8,4 +9,14 @@ function getNewDeck() {
     });
 }
 
+
+function newHand() {
+    fetch("https://deckofcardsapi.com/api/deck/ez4gc34z2q6z/draw/?count=5")
+    .then(data => data.json())
+    .then(newHand => {
+        hand = newHand.cards;
+    })
+}
+
+//newHand();
 //getNewDeck();
