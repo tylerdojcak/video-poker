9d5dff1fbfa9e8a9ce37e1300ddfd749c396997a
tyler <tyler.dojcak@gmail.com>
2021-03-16 13:34:25 -0400

    shuffle the deck before drawing a new hand

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 let deckID;
 let hand;
+let handDiv = document.querySelector(".hand");
 
+// GET A BRAND NEW DECK WITH UNIQUE IDENTIFIER
 function getNewDeck() {
     fetch("https://deckofcardsapi.com/api/deck/new/shuffle/")
     .then(data => data.json())
@@ -9,13 +11,32 @@ function getNewDeck() {
     });
 }
 
-
-function newHand() {
-    fetch("https://deckofcardsapi.com/api/deck/ez4gc34z2q6z/draw/?count=5")
+// SHUFFLE THE DECK AND DRAW A 5-CARD HAND, THEN DISPLAY THEM ON-SCREEN
+async function newHand() {
+    //await shuffleDeck();
+    await fetch(`https://deckofcardsapi.com/api/deck/ez4gc34z2q6z/shuffle/`);
+    fetch(`https://deckofcardsapi.com/api/deck/ez4gc34z2q6z/draw/?count=5`)
     .then(data => data.json())
     .then(newHand => {
         hand = newHand.cards;
-    })
+        handDiv.innerHTML = "";
+        for (let card of hand) {
+            createCardDiv(card)
+        }
+    });
+}
+
+/*function shuffleDeck() {
+    fetch(`https://deckofcardsapi.com/api/deck/ez4gc34z2q6z/shuffle/`)
+    .then(data => data.json());
+}*/
+
+function createCardDiv(card) {
+    let newCard = document.createElement("div");
+    newCard.setAttribute("id", card.code)
+    newCard.setAttribute("class", "card");
+    newCard.style.backgroundImage = `url(${card.image})`;
+    handDiv.appendChild(newCard);
 }
 
 //newHand();
